a354effc74a2d8cc9dcd7e6ec465ee520f3f3a4f
jessicasousa <jessicacardosops@gmail.com>
2017-10-16 19:33:35 -0300

    test

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+#' Hello, world!
+#
+#' This is an example function named 'hello'
+#' which prints 'Hello, name'.
+#' @param name string
+#'
+#' @export
+hello <- function(name) {
+  print(paste("Hello,",name))
+}
